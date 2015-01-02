56f160d27cae6509248625322f39ce5570df1d88
Kian Moretz <meecoder@users.noreply.github.com>
2015-01-02 16:07:05 -0500

    Fix a small bug

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('a').fadeOut();
     $('p').fadeOut();
-    $('input').keydown(function () {
+    $('input').keyup(function () {
         for (var i = 0; i <= $('a').length * 2; i++) {
             if($('a:nth-child(' + i + ')').data('price') <= $('input').val()) {
                 $('a:nth-child(' + i + ')').fadeIn();
